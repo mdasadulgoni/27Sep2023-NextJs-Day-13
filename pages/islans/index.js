4def407879b57ff02217c00b9f4e4a274658ab57
mdasadulgoni <mdasadulgoni@gmail.com>
2023-09-27 22:32:13 +0530

    c2

--- pages/islans/index.js
+++ pages/islans/index.js
@@ -7,7 +7,7 @@ import React from 'react'
     <>
       <section className='col-6 mt-2'>
         <h1>The Island</h1>
-        <p>ere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lore</p>
+        <p>ere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lore</p>
       </section>
     </>
   )
