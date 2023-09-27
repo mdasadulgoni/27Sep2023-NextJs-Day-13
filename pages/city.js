96b21f7d140d2f40d4a0d84523472f5d57551886
mdasadulgoni <mdasadulgoni@gmail.com>
2023-09-27 20:49:38 +0530

    c1

--- pages/city.js
+++ pages/city.js
@@ -5,6 +5,7 @@ import React from 'react'
  function city() {
   return (
     <>
+    you&apos;re free
       <section className='col-6 mt-2'>
         <h1>The City</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but</p>
